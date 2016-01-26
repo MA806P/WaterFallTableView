5fb0003b090d2973abcc1600e39ac26df6b9663c
MA806P <mayp126@126.com>
2016-01-26 17:06:08 +0800

    add new file

---
+++ WaterFallTableView/WaterFallTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaterFallTableView
+//
+//  Created by qianfeng on 15/7/2.
+//  Copyright (c) 2015年 MaYupeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaterFallTableView/WaterFallTableView/YPViewController.h
@@ -0,0 +1,13 @@
+//
+//  YPViewController.h
+//  WaterFallTableView
+//
+//  Created by qianfeng on 15/7/2.
+//  Copyright (c) 2015年 MaYupeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YPViewController : UIViewController
+
+@end
